f1c10dc84474debe81dd75a4fee4d7d0f6b2a701
jinwook song <wlsdnr129@naver.com>
2023-07-16 16:23:24 +0900

    tdd: stack

---
+++ src/stack/stack.js
@@ -0,0 +1,31 @@
+class Stack {
+  constructor() {
+    this.array = [];
+  }
+
+  size() {
+    return this.array.length;
+  }
+
+  push(item) {
+    this.array.push(item);
+  }
+
+  pop() {
+    if (this.array.length === 0) {
+      throw new Error('Stack is empty');
+    } else {
+      return this.array.pop();
+    }
+  }
+
+  peek() {
+    if (this.array.length === 0) {
+      throw new Error('Stack is empty');
+    } else {
+      return this.array.at(-1);
+    }
+  }
+}
+
+module.exports = Stack;
